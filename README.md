02964892cc5f2dd452f3e07b9807a7837d073dbb
HPNeo <hpneo@hotmail.com>
2012-03-05 18:44:26 -0500

    Added callbacks

--- README.md
+++ README.md
@@ -5,7 +5,14 @@ Folio.js is a jQuery plugin that creates a stack of views and pages using your H
 
 Usage
 -----
-    var folio = $('#slide_wrapper').folio();
+    var folio = $('#slide_wrapper').folio(options);
+
+`options` is a hash of settings:
+
+* viewClass: class name used to define views, `.view` by default.
+* pageClass:  class name used to define pages, `.page` by default.
+* afterSlideView: callback that will fire after slide to another view.
+* afterSlidePage: callback that will fire after slide to another page.
 
 `folio` will return `$('#slide_wrapper')` with some extra methods, like:
 
--- folio.js
+++ folio.js
@@ -1,6 +1,6 @@
 (function($){
   function getcurrentView(parent, viewClass){
-    return $(parent.find(viewClass+'.current'));
+    return parent.find(viewClass+'.current');
   }
 
   $.fn.folio = function(options){
@@ -9,11 +9,13 @@
     var settings = $.extend({
       viewClass: '.view',
       pageClass: '.page',
-      parent: this
+      parent: this,
+      afterSlideView: function(){},
+      afterSlidePage: function(){}
     }, options);
 
     this.views = parent.find(settings.viewClass).map(function(index, item){
-      return $(item).view({parent: parent});
+      return $(item).view($.extend(settings, {parent: parent}));
     });
 
     this.addView = function(view, args){
@@ -27,7 +29,7 @@
 
     this.initViews = function(){
       this.views = $(this.views).map(function(index, item){
-        item = item.view({parent: parent});
+        item = item.view($.extend(settings, {parent: parent}));
 
         item.css('top', parent.height()*index);
 
@@ -90,14 +92,20 @@
         complete: function(){
           nextView.addClass('current');
           currentView.removeClass('current');
-          parent.currentView = getcurrentView(parent, settings.viewClass).view({parent: parent});
+          parent.currentView = getcurrentView(parent, settings.viewClass).view($.extend(settings, {parent: parent}));
           parent.currentView.initPages();
+
+          e = {};
+          e.currentView = $(parent.currentView);
+          e.currentIndex = e.currentView.index();
+
+          settings.afterSlideView.apply(this, [e]);
         }
       });
     };
 
     this.initViews();
-    this.currentView = getcurrentView(parent, settings.viewClass).view({parent: parent});
+    this.currentView = getcurrentView(parent, settings.viewClass).view($.extend(settings, {parent: parent}));
 
     return this;
   };
@@ -105,7 +113,7 @@
 
 (function($){
   function getcurrentPage(parent, pageClass){
-    return $(parent.find(pageClass+'.current'));
+    return parent.find(pageClass+'.current');
   }
 
   $.fn.view = function(options){
@@ -118,7 +126,7 @@
     }, options);
 
     this.pages = self.find(settings.pageClass).map(function(index, item){
-      return $(item).page({parent: self});
+      return $(item).page($.extend(settings, {parent: self}));
     });
 
     this.addPage = function(page, args){
@@ -187,14 +195,19 @@
         complete: function(){
           nextPage.addClass('current');
           currentPage.removeClass('current');
-          self.currentPage = getcurrentPage(self, settings.pageClass).page({parent: self});
+          self.currentPage = getcurrentPage(self, settings.pageClass).page($.extend(settings, {parent: self}));
+
+          e = {};
+          e.currentPage = $(self.currentPage);
+          e.currentIndex = e.currentPage.index();
+          settings.afterSlidePage.apply(this, [e]);
         }
       });
     };
 
     this.initPages = function(){
       this.pages = $(this.pages).map(function(index, item){
-        item = item.page({parent: parent});
+        item = item.page($.extend(settings, {parent: self}));
 
         item.css('left', settings.parent.width()*index);
 
@@ -205,7 +218,7 @@
     };
 
     this.initPages();
-    this.currentPage = getcurrentPage(self, settings.pageClass).page({parent: self});
+    this.currentPage = getcurrentPage(self, settings.pageClass).page($.extend(settings, {parent: self}));
 
     return this;
   };
@@ -218,3 +231,136 @@
     return this;
   }
 })(jQuery);
+
+$.extend($.easing,
+{
+  def: 'easeOutQuad',
+  swing: function (x, t, b, c, d) {
+      //alert($.easing.default);
+      return $.easing[$.easing.def](x, t, b, c, d);
+  },
+  easeInQuad: function (x, t, b, c, d) {
+      return c*(t/=d)*t + b;
+  },
+  easeOutQuad: function (x, t, b, c, d) {
+      return -c *(t/=d)*(t-2) + b;
+  },
+  easeInOutQuad: function (x, t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t + b;
+      return -c/2 * ((--t)*(t-2) - 1) + b;
+  },
+  easeInCubic: function (x, t, b, c, d) {
+      return c*(t/=d)*t*t + b;
+  },
+  easeOutCubic: function (x, t, b, c, d) {
+      return c*((t=t/d-1)*t*t + 1) + b;
+  },
+  easeInOutCubic: function (x, t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t*t + b;
+      return c/2*((t-=2)*t*t + 2) + b;
+  },
+  easeInQuart: function (x, t, b, c, d) {
+      return c*(t/=d)*t*t*t + b;
+  },
+  easeOutQuart: function (x, t, b, c, d) {
+      return -c * ((t=t/d-1)*t*t*t - 1) + b;
+  },
+  easeInOutQuart: function (x, t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+      return -c/2 * ((t-=2)*t*t*t - 2) + b;
+  },
+  easeInQuint: function (x, t, b, c, d) {
+      return c*(t/=d)*t*t*t*t + b;
+  },
+  easeOutQuint: function (x, t, b, c, d) {
+      return c*((t=t/d-1)*t*t*t*t + 1) + b;
+  },
+  easeInOutQuint: function (x, t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+      return c/2*((t-=2)*t*t*t*t + 2) + b;
+  },
+  easeInSine: function (x, t, b, c, d) {
+      return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+  },
+  easeOutSine: function (x, t, b, c, d) {
+      return c * Math.sin(t/d * (Math.PI/2)) + b;
+  },
+  easeInOutSine: function (x, t, b, c, d) {
+      return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+  },
+  easeInExpo: function (x, t, b, c, d) {
+      return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+  },
+  easeOutExpo: function (x, t, b, c, d) {
+      return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+  },
+  easeInOutExpo: function (x, t, b, c, d) {
+      if (t==0) return b;
+      if (t==d) return b+c;
+      if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+      return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+  },
+  easeInCirc: function (x, t, b, c, d) {
+      return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+  },
+  easeOutCirc: function (x, t, b, c, d) {
+      return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+  },
+  easeInOutCirc: function (x, t, b, c, d) {
+      if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+      return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+  },
+  easeInElastic: function (x, t, b, c, d) {
+      var s=1.70158;var p=0;var a=c;
+      if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+      if (a < Math.abs(c)) { a=c; var s=p/4; }
+      else var s = p/(2*Math.PI) * Math.asin (c/a);
+      return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+  },
+  easeOutElastic: function (x, t, b, c, d) {
+      var s=1.70158;var p=0;var a=c;
+      if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+      if (a < Math.abs(c)) { a=c; var s=p/4; }
+      else var s = p/(2*Math.PI) * Math.asin (c/a);
+      return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+  },
+  easeInOutElastic: function (x, t, b, c, d) {
+      var s=1.70158;var p=0;var a=c;
+      if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+      if (a < Math.abs(c)) { a=c; var s=p/4; }
+      else var s = p/(2*Math.PI) * Math.asin (c/a);
+      if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+      return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+  },
+  easeInBack: function (x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      return c*(t/=d)*t*((s+1)*t - s) + b;
+  },
+  easeOutBack: function (x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+  },
+  easeInOutBack: function (x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+      return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+  },
+  easeInBounce: function (x, t, b, c, d) {
+      return c - $.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+  },
+  easeOutBounce: function (x, t, b, c, d) {
+      if ((t/=d) < (1/2.75)) {
+          return c*(7.5625*t*t) + b;
+      } else if (t < (2/2.75)) {
+          return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+      } else if (t < (2.5/2.75)) {
+          return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+      } else {
+          return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+      }
+  },
+  easeInOutBounce: function (x, t, b, c, d) {
+      if (t < d/2) return $.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+      return $.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+  }
+});
